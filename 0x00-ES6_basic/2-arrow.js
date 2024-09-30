109dc50bdc377849981df8624ea965b48bf35186
hakimsmox <abdelhakimsmox@gmail.com>
2024-09-30 16:35:54 +0100

    2-arrow.js

--- 0x00-ES6_basic/2-arrow.js
+++ 0x00-ES6_basic/2-arrow.js
@@ -1,1 +1,8 @@
-#!/usr/bin/node
+export default function getNeighborhoodsList() {
+  this.sanFranciscoNeighborhoods = ['SOMA', 'Union Square'];
+
+  this.addNeighborhood = (newNeighborhood) => {
+    this.sanFranciscoNeighborhoods.push(newNeighborhood);
+    return this.sanFranciscoNeighborhoods;
+  };
+}

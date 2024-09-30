ab88a4664785c744d900d5519bae124ff28bb96d
hakimsmox <abdelhakimsmox@gmail.com>
2024-09-30 15:33:51 +0100

    1-block-scoped.js

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,11 +1,11 @@
 #!/usr/bin/node
 export default function taskBlock(trueOrFalse) {
-  const task = false;
-  const task2 = true;
+  let task = false;
+  let task2 = true;
 
   if (trueOrFalse) {
-    const task = true;
-    const task2 = false;
+    task = true;
+    task2 = false;
   }
 
   return [task, task2];

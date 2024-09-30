d1a93488cb3811a0b39a7198b56183e763ee80e8
hakimsmox <abdelhakimsmox@gmail.com>
2024-09-30 18:02:48 +0100

    10-loops.js

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -1,8 +1,7 @@
 export default function appendToEachArrayValue(array, appendString) {
+  const newArray = [];
   for (const value of array) {
-    const index = array.indexOf(value);
-    array[index] = appendString + value;
+    newArray.push(appendString + value);
   }
-
-  return array;
+  return newArray;
 }

9f82eca65434271f023b510274820da5bf6ebdc9
hakimsmox <abdelhakimsmox@gmail.com>
2024-10-01 17:44:03 +0100

    3-all.js

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,1 +1,14 @@
-#!/usr/bin/node
+import { uploadPhoto, createUser } from './utils';
+
+export default function handleProfileSignup() {
+  // Use Promise.all to handle both promises
+  return Promise.all([uploadPhoto(), createUser()])
+    .then(([photoResponse, userResponse]) => {
+      // Log the required information
+      console.log(`${photoResponse.body} ${userResponse.firstName} ${userResponse.lastName}`);
+    })
+    .catch(() => {
+      // Log error message if any promise fails
+      console.log('Signup system offline');
+    });
+}

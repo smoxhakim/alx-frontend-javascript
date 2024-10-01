7bddc7f4771624c0679ed0b1be64ef96eb3f0822
hakimsmox <abdelhakimsmox@gmail.com>
2024-10-01 21:15:41 +0100

    9-try.js

--- 0x01-ES6_promise/9-try.js
+++ 0x01-ES6_promise/9-try.js
@@ -1,1 +1,14 @@
-#!/usr/bin/node
+export default function guardrail(mathFunction) {
+  const queue = [];
+
+  try {
+    const result = mathFunction();
+    queue.push(result);
+  } catch (error) {
+    queue.push(error.message);
+  } finally {
+    queue.push('Guardrail was processed');
+  }
+
+  return queue;
+}

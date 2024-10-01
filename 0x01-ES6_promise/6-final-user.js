a2b613effe800f7a2b53b7305bd95f3b552a8fed
hakimsmox <abdelhakimsmox@gmail.com>
2024-10-01 20:26:08 +0100

    7-load_balancer.js

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,16 +1,13 @@
-import signUpUser from './4-user-promise'
-import uploadPhoto from './5-photo-reject'
+import signUpUser from './4-user-promise';
+import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lasName, fileName) {
-  
-  const userPromise = signUpUser(firstName, lasName)
-  const photoPromise = uploadPhoto(fileName)
+  const userPromise = signUpUser(firstName, lasName);
+  const photoPromise = uploadPhoto(fileName);
 
   return Promise.allSettled([userPromise, photoPromise])
-    .then(results => {
-      return results.map(result => ({
-        status: result.status,
-        value: result.status === 'fulfilled' ? result.value : result.reason
-      }))
-    })
+    .then((results) => results.map((result) => ({
+      status: result.status,
+      value: result.status === 'fulfilled' ? result.value : result.reason,
+    })));
 }

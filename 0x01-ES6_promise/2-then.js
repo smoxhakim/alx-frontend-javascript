b9aa9149e7a9873cdccb2716c828a7971a85bcdd
hakimsmox <abdelhakimsmox@gmail.com>
2024-10-01 17:27:15 +0100

    2-then.js

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,1 +1,11 @@
-#!/usr/bin/node
+export default function handleResponseFromAPI(promise) {
+  return promise
+    .then(() => {
+      console.log('Got a response from the API');
+      return {
+        status: 200,
+        body: 'seccess',
+      };
+    })
+    .catch(() => new Error());
+}

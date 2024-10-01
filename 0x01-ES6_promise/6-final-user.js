f0bedd4fd6c1d379a1073fd6717f2dec7feb1af2
hakimsmox <abdelhakimsmox@gmail.com>
2024-10-01 20:03:17 +0100

    6-final-user.js

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,1 +1,16 @@
-#!/usr/bin/node
+import signUpUser from './4-user-promise'
+import uploadPhoto from './5-photo-reject'
+
+export default function handleProfileSignup(firstName, lasName, fileName) {
+  
+  const userPromise = signUpUser(firstName, lasName)
+  const photoPromise = uploadPhoto(fileName)
+
+  return Promise.allSettled([userPromise, photoPromise])
+    .then(results => {
+      return results.map(result => ({
+        status: result.status,
+        value: result.status === 'fulfilled' ? result.value : result.reason
+      }))
+    })
+}

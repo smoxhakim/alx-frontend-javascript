7e9bcac9d1a293b25e092dbf4541b74e71558781
Abdeljalil Ouafi <abdeljalilouafi44@gmail.com>
2024-10-03 00:35:33 +0100

    update

--- 0x01-ES6_promise/100-await.js
+++ 0x01-ES6_promise/100-await.js
@@ -2,17 +2,9 @@ import { uploadPhoto, createUser } from './utils';
 
 export default async function asyncUploadUser() {
   try {
-    const photo = await uploadPhoto();
-    const user = await createUser();
-
-    return {
-      photo,
-      user,
-    };
-  } catch (error) {
-    return {
-      photo: null,
-      user: null,
-    };
+    const [photo, user] = await Promise.all([uploadPhoto(), createUser()]);
+    return { photo, user };
+  } catch (e) {
+    return { photo: null, user: null };
   }
 }

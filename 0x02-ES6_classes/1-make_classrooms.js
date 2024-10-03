3e12c676edaa0e2a23a71a51e3be46e1ce9b6359
hakimsmox <abdelhakimsmox@gmail.com>
2024-10-03 21:55:48 +0100

    1-make_classrooms.js

--- 0x02-ES6_classes/1-make_classrooms.js
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -1,1 +1,9 @@
-#!/usr/bin/node
+import ClassRoom from './0-classroom.js'; 
+
+export default function initializeRooms() {
+  return [
+    new ClassRoom(19), 
+    new ClassRoom(20), 
+    new ClassRoom(34)  
+  ];
+}

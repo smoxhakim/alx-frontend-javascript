1b5e10657bfd509a988fe8471d9416168d796d72
hakimsmox <abdelhakimsmox@gmail.com>
2024-10-03 22:18:14 +0100

    5-building.js

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -1,6 +1,9 @@
 export default class Building {
   constructor(sqft) {
     this._sqft = sqft;
+    if (new.target === Building) {
+      throw new Error('Cannot instantiate abstract class Building directly');
+    }
   }
 
   get sqft() {

bc4f34e960490d1aa041356024614fdaa9752efc
hakimsmox <abdelhakimsmox@gmail.com>
2024-10-03 22:56:50 +0100

    10-car.js

--- 0x02-ES6_classes/10-main.js
+++ 0x02-ES6_classes/10-main.js
@@ -1,12 +1,14 @@
-export default class Car {
-    constructor(brand, motor, color) {
-      this._brand = brand;
-      this._motor = motor;
-      this._color = color;
-    }
-  
-    cloneCar() {
-      const Constructor = Object.getPrototypeOf(this).constructor;
-      return new Constructor();
-    }
-  }
+import Car from "./10-car.js";
+
+class TestCar extends Car {}
+
+const tc1 = new TestCar("Nissan", "Turbo", "Pink");
+const tc2 = tc1.cloneCar();
+
+console.log(tc1);
+console.log(tc1 instanceof TestCar);
+
+console.log(tc2);
+console.log(tc2 instanceof TestCar);
+
+console.log(tc1 == tc2);

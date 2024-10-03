2e2794aea24b610f70f363db5fe3b77a0bd31c29
hakimsmox <abdelhakimsmox@gmail.com>
2024-10-03 22:32:06 +0100

    8-hbtn_class.js

--- 0x02-ES6_classes/8-hbtn_class.js
+++ 0x02-ES6_classes/8-hbtn_class.js
@@ -1,1 +1,14 @@
-#!/usr/bin/node
+export default class HolbertonClass {
+  constructor(size, location) {
+    this._size = size;
+    this._location = location;
+  }
+
+  valueOf() {
+    return this._size;
+  }
+
+  toString() {
+    return this._location;
+  }
+}

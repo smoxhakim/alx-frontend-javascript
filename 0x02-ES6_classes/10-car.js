a39cb6f148d6d5df1787ea2de49a571b247f603d
hakimsmox <abdelhakimsmox@gmail.com>
2024-10-03 22:35:34 +0100

    10-car.js

--- 0x02-ES6_classes/10-car.js
+++ 0x02-ES6_classes/10-car.js
@@ -1,1 +1,20 @@
-#!/usr/bin/node
+const _brand = Symbol('brand');
+const _motor = Symbol('motor');
+const _color = Symbol('color');
+
+export default class Car {
+  constructor(brand, motor, color) {
+    this[_brand] = brand;
+    this[_motor] = motor;
+    this[_color] = color;
+  }
+
+  cloneCar() {
+    const Constructor = this.constructor[Symbol.species] || this.constructor;
+    return new Constructor();
+  }
+
+  static get [Symbol.species]() {
+    return this;
+  }
+}

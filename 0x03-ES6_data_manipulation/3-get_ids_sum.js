d0da4d66b7bfec7c4f972d8889ca214275541040
hakimsmox <abdelhakimsmox@gmail.com>
2024-10-07 20:09:12 +0100

    3-get_ids_sum.js

--- 0x03-ES6_data_manipulation/3-get_ids_sum.js
+++ 0x03-ES6_data_manipulation/3-get_ids_sum.js
@@ -1,1 +1,6 @@
-#!/usr/bin/node
+export default function getStudentIdsSum(studentList) {
+  if (!Array.isArray(studentList)) {
+    return 0;
+  }
+  return studentList.reduce((accumulator, student) => accumulator + student.id, 0);
+}

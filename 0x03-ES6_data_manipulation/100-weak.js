ab275b12a8fb0455a6030d86df43b2a4ab6f9b39
hakimsmox <abdelhakimsmox@gmail.com>
2024-10-08 16:30:49 +0100

    100-weak.js

--- 0x03-ES6_data_manipulation/100-weak.js
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -1,1 +1,14 @@
-#!/usr/bin/node
+export const weakMap = new WeakMap();
+
+export function queryAPI(endpoint) {
+  if (!weakMap.has(endpoint)) {
+    weakMap.set(endpoint, 0);
+  }
+
+  const currentCount = weakMap.get(endpoint) + 1;
+  weakMap.set(endpoint, currentCount);
+
+  if (currentCount >= 5) {
+    throw new Error('Endpoint load is high');
+  }
+}

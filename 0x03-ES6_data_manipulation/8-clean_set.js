90e6e4838cd48c15f05deb55cfdb3c6e14748392
hakimsmox <abdelhakimsmox@gmail.com>
2024-10-08 15:46:16 +0100

    8-clean_set.js

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,1 +1,15 @@
-#!/usr/bin/node
+export default function cleanSet(set, startString) {
+  if (startString === '') {
+    return '';
+  }
+
+  const result = [];
+
+  for (const item of set) {
+    if (item.startsWith(startString)) {
+      result.push(item.slice(startString.length));
+    }
+  }
+
+  return result.join('-');
+}

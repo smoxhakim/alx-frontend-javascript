abff6f453394106e30c357f1c6f4f66c93f9bcc6
hakimsmox <abdelhakimsmox@gmail.com>
2024-10-08 15:06:11 +0100

    4-update_grade_by_city.js

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,1 +1,13 @@
-#!/usr/bin/node
+export default function updateStudentGradeByCity(students, city, newGrades) {
+  return students
+    .filter((student) => student.location === city)
+    .map((student) => {
+      const gradeObject = newGrades.find((grade) => grade.studentId === student.id);
+      return {
+        id: student.id,
+        firstName: student.firstName,
+        location: student.location,
+        grade: gradeObject ? gradeObject.grade : 'N/A',
+      };
+    });
+}

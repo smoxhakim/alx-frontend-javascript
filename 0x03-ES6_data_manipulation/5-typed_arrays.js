606386bdea2d7f352cae7d518e53a4f024c682e6
hakimsmox <abdelhakimsmox@gmail.com>
2024-10-08 15:21:31 +0100

    5-typed_arrays.js

--- 0x03-ES6_data_manipulation/5-typed_arrays.js
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -1,1 +1,11 @@
-#!/usr/bin/node
+export default function createInt8TypedArray(length, position, value) {
+  if (position < 0 || position >= length) {
+    throw new Error('Position outside range');
+  }
+
+  const buffer = new ArrayBuffer(length);
+  const int8View = new Int8Array(buffer);
+
+  int8View[position] = value;
+  return new DataView(buffer);
+}
